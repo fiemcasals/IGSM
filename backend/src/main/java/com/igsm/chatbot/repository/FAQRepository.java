b19f4e4f9411962f984984d885438bd968be3b05
mcasals <mcasals@fie.undef.edu.ar>
2026-02-07 11:49:58 -0300

    mix error

--- backend/src/main/java/com/igsm/chatbot/repository/FAQRepository.java
+++
@@ -1,9 +0,0 @@
-package com.igsm.chatbot.repository;
-
-import com.igsm.chatbot.model.FAQ;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FAQRepository extends JpaRepository<FAQ, Long> {
-}
